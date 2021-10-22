ab41083f9e40dd63200039bf7c12ef01788e2624
rahimin10 <rahimin710@gmail.com>
2021-10-22 21:15:19 +0700

    push problem 8

---
+++ Assets/Scripts/UIScoreControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScoreControl : MonoBehaviour
+{
+    public Text score;
+	public ScoreControl scoreControl;
+	void Start()
+	{
+
+	}
+	void Update()
+	{
+		score.text = scoreControl.GetCurrentScore().ToString();
+	}
+}
