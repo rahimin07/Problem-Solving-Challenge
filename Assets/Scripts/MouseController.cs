f16059049be9008b6bdd6006ae0971adb1944540
rahimin10 <rahimin710@gmail.com>
2021-10-22 19:47:50 +0700

    6

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+	private Rigidbody2D playerRigibody;
+	public Camera mainCam;
+	public float speed;
+	public ScoreControl score;
+    private void Start()
+    {
+        playerRigibody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cursorPlayer = mainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,1));
+       	Vector3 angle = cursorPlayer - transform.position; 
+    	if (Vector3.Distance(cursorPlayer, transform.position) > 0.5)
+    	{
+    		transform.Translate(angle.normalized *speed* Time.deltaTime);
+    	}
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+    	if (collision.gameObject.tag == "Spawn")
+    	{
+    		score.IncrementScore();
+    		Destroy(collision.gameObject);
+    	}
+    }
+}
