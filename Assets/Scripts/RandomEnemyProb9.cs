82416a5101daed67c6a320541515035758db5c28
rahimin10 <rahimin710@gmail.com>
2021-10-22 21:38:59 +0700

    Push problem10

---
+++ Assets/Scripts/RandomEnemyProb9.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class RandomEnemyProb9 : MonoBehaviour
+    {     
+        public GameObject segienam;                
+        public float xMin, xMax, yMin, yMax;        
+
+        void Start ()
+        {
+            Spawn();
+            StartCoroutine(SpawnSegienam());
+        }
+        void Spawn ()
+        {
+            int spawnEnemy = Random.Range (7, 14);
+            for (int i =0; i < spawnEnemy; i++)
+            {
+                float randomX = Random.Range(xMin, xMax);
+                float randomY = Random.Range(yMin, yMax);
+                Instantiate(segienam, new Vector2(randomX, randomY), Quaternion.identity);
+            }           
+        }
+        IEnumerator SpawnSegienam()
+        {
+            if (transform.childCount < 12)
+            {
+                float randomX = Random.Range(xMin, xMax);
+                float randomY = Random.Range(yMin, yMax);
+                Instantiate(segienam, new Vector2(randomX, randomY), Quaternion.identity);
+            }
+            yield return new WaitForSeconds(1);
+            StartCoroutine(SpawnSegienam());
+        }
+    }
---
+++ Assets/Scripts/ScoreControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreControl : MonoBehaviour
+{
+    private int score;
+    
+    public float GetCurrentScore()
+    {
+    	return score;
+    }
+    public void IncrementScore()
+    {
+    	score++;
+    }
+    public int Score
+    {
+    	get { return score; }
+    }
+}
