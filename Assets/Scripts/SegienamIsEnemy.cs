3e1e4a6ffd5c1f1aa2820d7b3c8b1afd7f3155d6
rahimin10 <rahimin710@gmail.com>
2021-10-22 21:33:43 +0700

    push problem9

---
+++ Assets/Scripts/SegienamIsEnemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+    public class SegienamIsEnemy : MonoBehaviour
+    {     
+        public GameObject segienam;                
+        public float xMin, xMax, yMin, yMax;        
+
+        void Start ()
+        {
+            Spawn();
+        }
+        void Spawn ()
+        {
+            int spawnEnemy = Random.Range (4, 12);
+            for (int i =0; i < spawnEnemy; i++)
+            {
+                float randomX = Random.Range(xMin, xMax);
+                float randomY = Random.Range(yMin, yMax);
+                Instantiate(segienam, new Vector2(randomX, randomY), Quaternion.identity);
+            }
+            
+        }
+    }
